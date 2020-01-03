c03268f2bc52e03dc48cc92348cdb65f49b45aaf
AhteshamulHaque <ahteshamul8900@gmail.com>
2020-01-03 20:18:29 +0530

    Added seperate sql files

---
+++ backup/credits.sql
@@ -0,0 +1,634 @@
+--
+-- Current Database: `nilekrator$credits`
+--
+
+CREATE DATABASE IF NOT EXISTS `nilekrator$CREDITS` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `nilekrator$CREDITS`;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_CACA`
+--
+
+DROP TABLE IF EXISTS `PG_CACA`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_CACA` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_CACA`
+--
+
+LOCK TABLES `PG_CACA` WRITE;
+/*!40000 ALTER TABLE `PG_CACA` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_CACA` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_CAIS`
+--
+
+DROP TABLE IF EXISTS `PG_CAIS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_CAIS` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_CAIS`
+--
+
+LOCK TABLES `PG_CAIS` WRITE;
+/*!40000 ALTER TABLE `PG_CAIS` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_CAIS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_CEGE`
+--
+
+DROP TABLE IF EXISTS `PG_CEGE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_CEGE` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_CEGE`
+--
+
+LOCK TABLES `PG_CEGE` WRITE;
+/*!40000 ALTER TABLE `PG_CEGE` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_CEGE` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_CESE`
+--
+
+DROP TABLE IF EXISTS `PG_CESE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_CESE` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_CESE`
+--
+
+LOCK TABLES `PG_CESE` WRITE;
+/*!40000 ALTER TABLE `PG_CESE` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_CESE` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_CEWR`
+--
+
+DROP TABLE IF EXISTS `PG_CEWR`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_CEWR` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_CEWR`
+--
+
+LOCK TABLES `PG_CEWR` WRITE;
+/*!40000 ALTER TABLE `PG_CEWR` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_CEWR` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_CHCH`
+--
+
+DROP TABLE IF EXISTS `PG_CHCH`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_CHCH` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_CHCH`
+--
+
+LOCK TABLES `PG_CHCH` WRITE;
+/*!40000 ALTER TABLE `PG_CHCH` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_CHCH` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_CHSS`
+--
+
+DROP TABLE IF EXISTS `PG_CHSS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_CHSS` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_CHSS`
+--
+
+LOCK TABLES `PG_CHSS` WRITE;
+/*!40000 ALTER TABLE `PG_CHSS` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_CHSS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_CSCS`
+--
+
+DROP TABLE IF EXISTS `PG_CSCS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_CSCS` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_CSCS`
+--
+
+LOCK TABLES `PG_CSCS` WRITE;
+/*!40000 ALTER TABLE `PG_CSCS` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_CSCS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_ECCO`
+--
+
+DROP TABLE IF EXISTS `PG_ECCO`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_ECCO` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_ECCO`
+--
+
+LOCK TABLES `PG_ECCO` WRITE;
+/*!40000 ALTER TABLE `PG_ECCO` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_ECCO` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_ECEM`
+--
+
+DROP TABLE IF EXISTS `PG_ECEM`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_ECEM` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_ECEM`
+--
+
+LOCK TABLES `PG_ECEM` WRITE;
+/*!40000 ALTER TABLE `PG_ECEM` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_ECEM` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_EEPE`
+--
+
+DROP TABLE IF EXISTS `PG_EEPE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_EEPE` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_EEPE`
+--
+
+LOCK TABLES `PG_EEPE` WRITE;
+/*!40000 ALTER TABLE `PG_EEPE` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_EEPE` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_EEPS`
+--
+
+DROP TABLE IF EXISTS `PG_EEPS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_EEPS` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_EEPS`
+--
+
+LOCK TABLES `PG_EEPS` WRITE;
+/*!40000 ALTER TABLE `PG_EEPS` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_EEPS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_MECI`
+--
+
+DROP TABLE IF EXISTS `PG_MECI`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_MECI` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_MECI`
+--
+
+LOCK TABLES `PG_MECI` WRITE;
+/*!40000 ALTER TABLE `PG_MECI` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_MECI` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_MEES`
+--
+
+DROP TABLE IF EXISTS `PG_MEES`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_MEES` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_MEES`
+--
+
+LOCK TABLES `PG_MEES` WRITE;
+/*!40000 ALTER TABLE `PG_MEES` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_MEES` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_METE`
+--
+
+DROP TABLE IF EXISTS `PG_METE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_METE` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_METE`
+--
+
+LOCK TABLES `PG_METE` WRITE;
+/*!40000 ALTER TABLE `PG_METE` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_METE` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_MFMS`
+--
+
+DROP TABLE IF EXISTS `PG_MFMS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_MFMS` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_MFMS`
+--
+
+LOCK TABLES `PG_MFMS` WRITE;
+/*!40000 ALTER TABLE `PG_MFMS` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_MFMS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_MHMH`
+--
+
+DROP TABLE IF EXISTS `PG_MHMH`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_MHMH` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_MHMH`
+--
+
+LOCK TABLES `PG_MHMH` WRITE;
+/*!40000 ALTER TABLE `PG_MHMH` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_MHMH` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_MMFT`
+--
+
+DROP TABLE IF EXISTS `PG_MMFT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_MMFT` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_MMFT`
+--
+
+LOCK TABLES `PG_MMFT` WRITE;
+/*!40000 ALTER TABLE `PG_MMFT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_MMFT` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_MMMT`
+--
+
+DROP TABLE IF EXISTS `PG_MMMT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_MMMT` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_MMMT`
+--
+
+LOCK TABLES `PG_MMMT` WRITE;
+/*!40000 ALTER TABLE `PG_MMMT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_MMMT` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `PG_PHPH`
+--
+
+DROP TABLE IF EXISTS `PG_PHPH`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PG_PHPH` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `PG_PHPH`
+--
+
+LOCK TABLES `PG_PHPH` WRITE;
+/*!40000 ALTER TABLE `PG_PHPH` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PG_PHPH` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `TYC_MME`
+--
+
+DROP TABLE IF EXISTS `TYC_MME`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `TYC_MME` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `TYC_MME`
+--
+
+LOCK TABLES `TYC_MME` WRITE;
+/*!40000 ALTER TABLE `TYC_MME` DISABLE KEYS */;
+/*!40000 ALTER TABLE `TYC_MME` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `UG_CE`
+--
+
+DROP TABLE IF EXISTS `UG_CE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UG_CE` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `UG_CE`
+--
+
+LOCK TABLES `UG_CE` WRITE;
+/*!40000 ALTER TABLE `UG_CE` DISABLE KEYS */;
+/*!40000 ALTER TABLE `UG_CE` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `UG_CS`
+--
+
+DROP TABLE IF EXISTS `UG_CS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UG_CS` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `UG_CS`
+--
+
+LOCK TABLES `UG_CS` WRITE;
+/*!40000 ALTER TABLE `UG_CS` DISABLE KEYS */;
+/*!40000 ALTER TABLE `UG_CS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `UG_EC`
+--
+
+DROP TABLE IF EXISTS `UG_EC`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UG_EC` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `UG_EC`
+--
+
+LOCK TABLES `UG_EC` WRITE;
+/*!40000 ALTER TABLE `UG_EC` DISABLE KEYS */;
+/*!40000 ALTER TABLE `UG_EC` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `UG_EE`
+--
+
+DROP TABLE IF EXISTS `UG_EE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UG_EE` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `UG_EE`
+--
+
+LOCK TABLES `UG_EE` WRITE;
+/*!40000 ALTER TABLE `UG_EE` DISABLE KEYS */;
+/*!40000 ALTER TABLE `UG_EE` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `UG_ME`
+--
+
+DROP TABLE IF EXISTS `UG_ME`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UG_ME` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `UG_ME`
+--
+
+LOCK TABLES `UG_ME` WRITE;
+/*!40000 ALTER TABLE `UG_ME` DISABLE KEYS */;
+/*!40000 ALTER TABLE `UG_ME` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `UG_MM`
+--
+
+DROP TABLE IF EXISTS `UG_MM`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UG_MM` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `UG_MM`
+--
+
+LOCK TABLES `UG_MM` WRITE;
+/*!40000 ALTER TABLE `UG_MM` DISABLE KEYS */;
+/*!40000 ALTER TABLE `UG_MM` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- TABLE STRUCTURE FOR TABLE `UG_PI`
+--
+
+DROP TABLE IF EXISTS `UG_PI`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UG_PI` (
+  `subject_code` varchar(10) DEFAULT NULL,
+  `credits` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- DUMPING DATA FOR TABLE `UG_PI`
+--
+
+LOCK TABLES `UG_PI` WRITE;
+/*!40000 ALTER TABLE `UG_PI` DISABLE KEYS */;
+/*!40000 ALTER TABLE `UG_PI` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-03 17:10:38
